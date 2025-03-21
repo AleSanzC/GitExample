e7a1aacf8ea5e9af8dc1697ed5452ead276e48f4
Alejandra Sanz Carrillo <alejandrasanz@uninorte.edu.co>
2025-03-21 16:04:54 -0500

    Feat Added Dog class

--- src/dog/Dog.java
+++ src/dog/Dog.java
@@ -2,27 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package core;
+package dog;
 
 /**
  *
  * @author alejandrasanz
  */
-public class Person {
+public class Dog {
     
-    private long id;
-    private String name;
     private int age;
+    private long name;
+    private float width;
+    private float height;
 
-    public Person(long id, String name, int age) {
-        this.id = id;
-        this.name = name;
+    public Dog(int age, long name, float width, float height) {
         this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Person(" + "id=" + id + ", name=" + name + ')';
+        this.name = name;
+        this.width = width;
+        this.height = height;
     }
     
     
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package main;
+
+import core.Person;
+
 
 /**
  *
@@ -11,8 +11,8 @@ package main;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Holii");
-   
+        Person person = new Person(1042250461, "Aleja Sanz", 18);
+        System.out.println(person);
     }
     
     
